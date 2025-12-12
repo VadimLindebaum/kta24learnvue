7bbe2151e689d8227a8efdfde0d255c72ac77428
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-12-12 14:05:34 +0200

    Add XMLHttpRequest example

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { createMemoryHistory, createWebHashHistory, createWebHistory, createRout
 
 import ToDo from './pages/ToDo.vue';
 import Modals from './pages/Modals.vue';
+import ChuckNorris from './pages/ChuckNorris.vue';
 
 const routes = [
   { path: '/', component: ToDo, name: 'ToDo' },
   { path: '/modals', component: Modals, name: 'Modals'},
+  { path: '/chuck', component: ChuckNorris, name: 'Chuck Norris'},
 ];
 
 export const router = createRouter({
