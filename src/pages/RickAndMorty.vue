8e7bf2ecb05b29a29a849071e4168e8073604151
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-15 15:00:02 +0200

    Add search

--- src/pages/RickAndMorty.vue
+++ src/pages/RickAndMorty.vue
@@ -6,13 +6,14 @@ import CharacterCard from '../components/CharacterCard.vue';
 let characters = ref([]);
 let info = ref({});
 let currentPage = ref(1);
+let searchValue = ref('');
 await getCharacters('https://rickandmortyapi.com/api/character');
 
 async function getCharacters(url) {
     let res = await axios.get(url);
     console.log(res);
 
-    characters.value = res.data.results;
+    characters.value.push(...res.data.results);
     info.value = res.data.info;
 }
 
@@ -33,43 +34,53 @@ async function getPage(page) {
 
 function makePages(pages, current) {
     let elements = [];
-    for(let i = 1; i<=3; i++){
+    for (let i = 1; i <= 3; i++) {
         elements[i] = i;
     }
-    if(current > 3) {
+    if (current > 3) {
         elements.push('...');
     }
-    for(let i = current-2; i<=current+2 && i<=pages; i++){
+    for (let i = current - 2; i <= current + 2 && i <= pages; i++) {
         elements[i] = i;
     }
 
-    if(current+2 < pages) {
+    if (current + 2 < pages) {
         elements.push('...');
     }
 
-    for(let i = pages-2; i<=pages; i++){
+    for (let i = pages - 2; i <= pages; i++) {
         elements[i] = i;
     }
 
-    return elements.filter(p => p);   
+    return elements.filter(p => p);
+}
+document.addEventListener('scroll', () => {
+    console.log(window.innerHeight + window.scrollY, document.body.clientHeight);
+    if (window.innerHeight + window.scrollY > document.body.clientHeight - 200) {
+        next();
+    }
+})
+
+async function search(){
+    characters.value = [];
+    await getCharacters('https://rickandmortyapi.com/api/character?name=' + searchValue.value);
 }
 
 
 </script>
 <template>
-    <div class="container">
-        <nav class="pagination is-centered" role="navigation" aria-label="pagination">
-            <button @click="prev" :disabled="!info.prev" class="pagination-previous">Previous</button>
-            <button @click="next" :disabled="!info.next" class="pagination-next">Next page</button>
-            <ul class="pagination-list">
-                <li v-for="page in makePages(info.pages, currentPage)">
-                    <span v-if="page === '...'" class="pagination-ellipsis">&hellip;</span>
-                    <button v-else-if="page === currentPage" class="pagination-link is-current" :aria-label="'Page ' + page" aria-current="page">{{ page }}</button>
-                    <button v-else @click="getPage(page)" class="pagination-link" :aria-label="'Goto page ' + page">{{ page }}</button>
-                </li>
-            </ul>
-        </nav>
 
+    <div class="container">
+        <div class="field has-addons">
+            <div class="control is-expanded">
+                <input v-model="searchValue" class="input" type="text" placeholder="Find a Character">
+            </div>
+            <div class="control">
+                <button @click="search" class="button is-info">
+                    Search
+                </button>
+            </div>
+        </div>
         <div class="columns is-multiline">
             <div class="column is-one-quarter" v-for="character in characters">
                 <CharacterCard :character="character"></CharacterCard>
