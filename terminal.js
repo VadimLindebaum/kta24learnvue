cfed58af09a983d2fcdeabf37c435f8a41e61cd8
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-29 14:10:00 +0200

    Add text styles and bell example

--- terminal.js
+++ terminal.js
@@ -4,12 +4,32 @@ function write(text) {
 
 const ESC = '\x1B';
 
-let time = new Date().toTimeString().substring(0,8);
-
-write(time);
-
+write(ESC + '[1m');
+write('Hello Terminal\n' );
+write(ESC + '[0m');
+write(ESC + '[2m');
+write('Hello Terminal\n' );
+write(ESC + '[0m');
+write(ESC + '[3m');
+write('Hello Terminal\n' );
+write(ESC + '[0m');
+write(ESC + '[4m');
+write('Hello Terminal\n');
+write(ESC + '[0m');
+write(ESC + '[5m');
+write('Hello Terminal\n');
+write(ESC + '[0m');
+write(ESC + '[7m');
+write('Hello Terminal\n');
+write(ESC + '[0m');
+write(ESC + '[8m');
+write('Hello Terminal\n');
+write(ESC + '[0m');
+write(ESC + '[9m');
+write('Hello Terminal\n' );
+write(ESC + '[0m');
+write(ESC + '[0m');
+write('Hello Terminal\n');
 setInterval(() => {
-    write(ESC+ '[8D');
-    time = new Date().toTimeString().substring(0,8);
-    write(time);
-},100);
+    write('\x07');
+},1000);
