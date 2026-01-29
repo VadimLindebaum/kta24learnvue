6ff01ca8352b6aed01e549c6e6f4c99fdaef661c
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-01-29 13:43:55 +0200

    Add truecolor

--- terminal.js
+++ terminal.js
@@ -5,7 +5,7 @@ function write(text) {
 const ESC = '\x1B';
 
 for(let i = 0; i<256; i++) {
-    write(ESC + `[48;5;${i}m`);
+    write(ESC + `[48;2;${i};${i};${i}m`);
     write(' ');
 }
 write(ESC + `[0m`);
